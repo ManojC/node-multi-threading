d009fdbc279df5f116a7b6bd3380a8969fec27f9
Manoj Chalode <chalodem@gmail.com>
2016-07-23 01:38:25 +0530

    added basic cluster module and replicated the multi threading session problem

--- app.js
+++ app.js
@@ -1,9 +1,77 @@
-const cluster = require('cluster');
-const http = require('http');
-const numCPUs = require('os').cpus().length;
+var express = require('express');
+var session = require('express-session');
+var path = require('path');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cluster = require('cluster');
+var http = require('http');
+
+var numCPUs = require('os').cpus().length;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave:true,
+    saveUninitialized: true 
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
 
 if (cluster.isMaster) {
     // Fork workers.
+    console.log(numCPUs);
+    console.log('master process is ', process.pid, '\n');
     for (var i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
@@ -14,8 +82,10 @@ if (cluster.isMaster) {
 } else {
     // Workers can share any TCP connection
     // In this case it is an HTTP server
-    http.createServer(function(req, res){
-        res.writeHead(200);
-        res.end('hello world\n');
-    }).listen(8000);
+    app.listen(3000, function() {
+        console.log('child process is ', process.pid, '\n');
+    });
 }
+
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/test', function(req, res, next) {
+    if (!req.session.asd) {
+        req.session.asd = 0;
+    }
+    ++req.session.asd;
+    req.session.save(function() {
+        res.jsonp({
+            data: 'john doe is smartest!',
+            pid: process.pid,
+            sessionId: req.cookies['connect.sid']
+        });
+    });
 });
 
 module.exports = router;
